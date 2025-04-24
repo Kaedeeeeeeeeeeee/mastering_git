1dafe3928b6be39888f0443a436b56059e6965ec
will <williyanto658@gmail.com>
2025-04-24 09:55:03 +0700

    adding new feature

--- tess2.js
+++ tess2.js
@@ -10,3 +10,18 @@ console.log('elo git')
 let names = will;
 console.log(`${names} is the dev`);
 
+let obeje = {
+    name: 'will',
+    age: 21,
+    job: 'dev'
+};
+
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    const data = await response.json();
+    console.log(data);
+};
+
+setTimeout(() =>{
+  alert('timeout')
+},3000);
